4531d521f4cfdbe7dd2c3de9bc9e67fc7d2258b1
Eliseo Rocha <rochaeliseo91@gmail.com>
2019-01-03 09:33:48 -0800

    Added a ReadKey to keep program open.

--- HelloSourceControl/Program.cs
+++ HelloSourceControl/Program.cs
@@ -11,7 +11,7 @@ namespace HelloSourceControl
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-
+            Console.ReadKey();
         }
     }
 }
